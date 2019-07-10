e3c15080fc0e6e0803cfb3c8bd383d3db978631b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-07-10 10:46:59 -0400

    testing included image with Electron and Chrome

--- included/test.sh
+++ included/test.sh
@@ -1,7 +1,9 @@
 set e+x
 
-echo Testing Docker image with cypress included
+echo "Testing Docker image with cypress included"
+echo "First, deleting existing dir /tmp/test if exists"
 
+rm -rf /tmp/test || true
 cd /tmp
 mkdir test
 cd test
@@ -9,4 +11,9 @@ npm init --yes
 npm i -D cypress
 npx @bahmutov/cly init
 rm -rf package-lock.json package.json node_modules
-docker run -it -v $PWD:/e2e -w /e2e cypress/included:3.3.2
+
+echo "Testing Electron browser"
+docker run -it -v $PWD:/e2e -w /e2e cypress/included:3.4.0
+
+echo "Testing Chrome browser"
+docker run -it -v $PWD:/e2e -w /e2e cypress/included:3.4.0 --browser chrome

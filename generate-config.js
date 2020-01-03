e36abb677e921695c177c7c5b87f784d3417f544
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-01-03 11:35:12 -0500

    WIP: Build browsers images on CI (#198)
    * move branch check to custom command
    
    * refactor Docker push command
    
    * trigger build
    
    * factor base workflow
    
    * more refactoring
    
    * factor check command
    
    * build browsers images

--- generate-config.js
+++ generate-config.js
@@ -1,3 +1,4 @@
+// @ts-check
 // this script generates CircleCI config file by looking at the "base/*" folders
 // for each subfolder it creates a separate job
 const globby = require('globby');
@@ -17,6 +18,95 @@ buildFilters: &buildFilters
         - master
         - add-circle-build
 
+commands:
+  halt-on-branch:
+    description: Halt current CircleCI job if not on master branch
+    steps:
+      - run:
+          name: Halting job if not on master branch
+          command: |
+            if [[ "$CIRCLE_BRANCH" != "master" ]]; then
+              echo "Not master branch, will skip the rest of commands"
+              circleci-agent step halt
+            else
+              echo "On master branch, can continue"
+            fi
+
+  halt-if-docker-image-exists:
+    description: Halt current CircleCI job if Docker image exists already
+    parameters:
+      imageName:
+        type: string
+        description: Docker image name to test
+    steps:
+      - run:
+          name: Check if image << parameters.imageName >> exists
+          # using https://github.com/mishguruorg/docker-image-exists
+          # to check if Docker hub has the image already
+          command: |
+            if npx docker-image-exists --quiet --repo << parameters.imageName >>; then
+              echo Found image << parameters.imageName >>
+              circleci-agent step halt
+            else
+              echo Did not find Docker image << parameters.imageName >>
+            fi
+
+  test-base-image:
+    description: Build a test image from base image and test it
+    parameters:
+      imageName:
+        type: string
+        description: Cypress base docker image to test
+    steps:
+      - run:
+          name: test image << parameters.imageName >>
+          command: |
+            docker build -t cypress/test -\\<<EOF
+            FROM << parameters.imageName >>
+            RUN echo "current user: $(whoami)"
+            ENV CI=1
+            RUN npm init --yes
+            RUN npm install --save-dev cypress
+            RUN ./node_modules/.bin/cypress verify
+            RUN npx @bahmutov/cly init
+            RUN ./node_modules/.bin/cypress run
+            EOF
+
+  test-browser-image:
+    description: Build a test image from browser image and test it
+    parameters:
+      imageName:
+        type: string
+        description: Cypress browser docker image to test
+    steps:
+      - run:
+          name: test image << parameters.imageName >>
+          # for now assuming Chrome, in the future can pass browser name as a parameter
+          command: |
+            docker build -t cypress/test -\\<<EOF
+            FROM << parameters.imageName >>
+            RUN echo "current user: $(whoami)"
+            ENV CI=1
+            RUN npm init --yes
+            RUN npm install --save-dev cypress
+            RUN ./node_modules/.bin/cypress verify
+            RUN npx @bahmutov/cly init
+            RUN ./node_modules/.bin/cypress run --browser chrome
+            EOF
+
+  docker-push:
+    description: Log in and push a given image to Docker hub
+    parameters:
+      imageName:
+        type: string
+        description: Docker image name to push
+    steps:
+      - run:
+          name: Pushing image << parameters.imageName >> to Docker Hub
+          command: |
+            echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+            docker push << parameters.imageName >>
+
 jobs:
   build-base-image:
     machine: true
@@ -30,61 +120,52 @@ jobs:
         description: Image tag to build like "12.14.0"
     steps:
       - checkout
-      - run:
-          name: Check if image exists
-          # using https://github.com/mishguruorg/docker-image-exists
-          # to check if Docker hub has the image already
-          command: |
-            if npx docker-image-exists --quiet --repo << parameters.dockerName >>:<< parameters.dockerTag >>; then
-              echo Found image << parameters.dockerName >>:<< parameters.dockerTag >>
-              circleci-agent step halt
-            else
-              echo Did not find Docker image << parameters.dockerName >>:<< parameters.dockerTag >>
-            fi
+      - halt-if-docker-image-exists:
+          imageName: << parameters.dockerName >>:<< parameters.dockerTag >>
       - run:
           name: building Docker image << parameters.dockerName >>:<< parameters.dockerTag >>
           command: |
             docker build -t << parameters.dockerName >>:<< parameters.dockerTag >> .
           working_directory: base/<< parameters.dockerTag >>
 
-      - run:
-          name: test built image
-          command: |
-            docker build -t cypress/test -\\<<EOF
-            FROM << parameters.dockerName >>:<< parameters.dockerTag >>
-            RUN echo "current user: $(whoami)"
-            ENV CI=1
-            RUN npm init --yes
-            RUN npm install --save-dev cypress
-            RUN ./node_modules/.bin/cypress verify
-            RUN npx @bahmutov/cly init
-            RUN ./node_modules/.bin/cypress run
-            EOF
+      - test-base-image:
+          imageName: << parameters.dockerName >>:<< parameters.dockerTag >>
+      - halt-on-branch
+      - docker-push:
+          imageName: << parameters.dockerName >>:<< parameters.dockerTag >>
 
+  build-browser-image:
+    machine: true
+    parameters:
+      dockerName:
+        type: string
+        description: Image name to build
+        default: cypress/browsers
+      dockerTag:
+        type: string
+        description: Image tag to build like "node12.4.0-chrome76"
+    steps:
+      - checkout
+      - halt-if-docker-image-exists:
+          imageName: << parameters.dockerName >>:<< parameters.dockerTag >>
       - run:
-          name: Check if can push to Docker Hub
+          name: building Docker image << parameters.dockerName >>:<< parameters.dockerTag >>
           command: |
-            if [[ "$CIRCLE_BRANCH" != "master" ]]; then
-              echo "Not master branch, will skip pushing newly built Docker image"
-              circleci-agent step halt
-            else
-              echo "On master branch, pushing Docker image to hub"
-            fi
+            docker build -t << parameters.dockerName >>:<< parameters.dockerTag >> .
+          working_directory: browsers/<< parameters.dockerTag >>
 
-      - run:
-          name: Pushing new image to Docker Hub
-          command: |
-            echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-            docker push << parameters.dockerName >>:<< parameters.dockerTag >>
+      - test-browser-image:
+          imageName: << parameters.dockerName >>:<< parameters.dockerTag >>
+      - halt-on-branch
+      - docker-push:
+          imageName: << parameters.dockerName >>:<< parameters.dockerTag >>
 
 workflows:
   version: 2
-  build-base-images:
-    jobs:
 `
 
-const writeConfigFile = (images) => {
-  const yml = images.map(imageAndTag => {
+const formBaseWorkflow = (baseImages) => {
+  const yml = baseImages.map(imageAndTag => {
     // important to have indent
     const job = '      - build-base-image:\n' +
       `          name: "${imageAndTag.tag}"\n` +
@@ -92,22 +173,59 @@ const writeConfigFile = (images) => {
       '          <<: *buildFilters\n'
     return job
   })
-  const text = preamble + yml.join('')
+
+  // indent is important
+  const workflowName = '  build-base-images:\n' +
+    '    jobs:\n'
+
+  const text = workflowName + yml.join('')
+  return text
+}
+
+const formBrowserWorkflow = (browserImages) => {
+  const yml = browserImages.map(imageAndTag => {
+    // important to have indent
+    const job = '      - build-browser-image:\n' +
+      `          name: "${imageAndTag.tag}"\n` +
+      `          dockerTag: "${imageAndTag.tag}"\n` +
+      '          <<: *buildFilters\n'
+    return job
+  })
+
+  // indent is important
+  const workflowName = '  build-browser-images:\n' +
+    '    jobs:\n'
+
+  const text = workflowName + yml.join('')
+  return text
+}
+
+const writeConfigFile = (baseImages, browserImages) => {
+  const base = formBaseWorkflow(baseImages)
+  const browsers = formBrowserWorkflow(browserImages)
+  const text = preamble + base + browsers
   fs.writeFileSync('circle.yml', text, 'utf8')
   console.log('generated circle.yml')
 }
 
-(async () => {
-	const paths = await globby('base/*', {onlyDirectories: true});
+const splitImageFolderName = (folderName) => {
+  const [name, tag] = folderName.split('/')
+  return {
+    name,
+    tag
+  }
+}
 
-  console.log(paths);
-  const namePlusTag = paths.map(path => {
-    const [name, tag] = path.split('/')
-    return {
-      name,
-      tag
-    }
-  })
-  console.log(namePlusTag)
-  writeConfigFile(namePlusTag)
+(async () => {
+  const basePaths = await globby('base/*', {onlyDirectories: true});
+  const base = basePaths.map(splitImageFolderName)
+  console.log(' *** base images ***')
+  console.log(base)
+
+  const browsersPaths = await globby('browsers/*', {onlyDirectories: true});
+  const browsers = browsersPaths.map(splitImageFolderName)
+  console.log(' *** browser images ***')
+  console.log(browsers)
+
+  writeConfigFile(base, browsers)
 })();

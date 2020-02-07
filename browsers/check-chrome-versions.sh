c1264959845a6a62322cc55c6fcf40745a1d9554
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-07 01:03:01 -0500

    check more images

--- browsers/check-chrome-versions.sh
+++ browsers/check-chrome-versions.sh
@@ -2,6 +2,36 @@
 
 docker pull cypress/browsers:node8.9.3-npm6.10.1-chrome76-ff68
 docker run cypress/browsers:node8.9.3-npm6.10.1-chrome76-ff68 google-chrome --version
+echo ""
+
+docker pull cypress/browsers:node12.0.0-chrome75
+docker run cypress/browsers:node12.0.0-chrome75 google-chrome --version
+echo ""
+
+docker pull cypress/browsers:node12.4.0-chrome76
+docker run cypress/browsers:node12.4.0-chrome76 google-chrome --version
+echo ""
+
+docker pull cypress/browsers:node12.6.0-chrome75
+docker run cypress/browsers:node12.6.0-chrome75 google-chrome --version
+echo ""
+
+docker pull cypress/browsers:node12.6.0-chrome77
+docker run cypress/browsers:node12.6.0-chrome77 google-chrome --version
+echo ""
+
+docker pull cypress/browsers:node12.8.1-chrome78-ff70
+docker run cypress/browsers:node12.8.1-chrome78-ff70 google-chrome --version
+echo ""
+
+docker pull cypress/browsers:node12.13.0-chrome78-ff70
+docker run cypress/browsers:node12.13.0-chrome78-ff70 google-chrome --version
+echo ""
 
 docker pull cypress/browsers:node13.3.0-chrome79-ff70
 docker run cypress/browsers:node13.3.0-chrome79-ff70 google-chrome --version
+echo ""
+
+docker pull cypress/browsers:node13.6.0-chrome-80-ff72
+docker run cypress/browsers:node13.6.0-chrome-80-ff72 google-chrome --version
+echo ""

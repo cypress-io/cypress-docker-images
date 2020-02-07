77c2730737e3aee37d2741cc4efc70d71c9c272d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-07 00:58:23 -0500

    check 79

--- browsers/check-chrome-versions.sh
+++ browsers/check-chrome-versions.sh
@@ -3,5 +3,5 @@
 docker pull cypress/browsers:node8.9.3-npm6.10.1-chrome76-ff68
 docker run cypress/browsers:node8.9.3-npm6.10.1-chrome76-ff68 google-chrome --version
 
-docker pull cypress/browsers:node13.3.0-chrome-79-ff70
-docker run cypress/browsers:node13.3.0-chrome-79-ff70 google-chrome --version
+docker pull cypress/browsers:node13.3.0-chrome79-ff70
+docker run cypress/browsers:node13.3.0-chrome79-ff70 google-chrome --version

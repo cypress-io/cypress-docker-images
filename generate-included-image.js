025dc5cf124b880d5cf90687f4b872700fb3e2ba
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-03-02 09:29:09 -0500

    add note about shared memory (#271)

--- generate-included-image.js
+++ generate-included-image.js
@@ -43,6 +43,12 @@ FROM ${baseImageTag}
 # https://github.com/cypress-io/cypress/issues/1243
 ENV CI=1
 
+# disable shared memory X11 affecting Cypress v4 and Chrome
+# https://github.com/cypress-io/cypress-docker-images/issues/270
+ENV QT_X11_NO_MITSHM=1
+ENV _X11_NO_MITSHM=1
+ENV _MITSHM=0
+
 # should be root user
 RUN echo "whoami: $(whoami)"
 RUN npm config -g set user $(whoami)

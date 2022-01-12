d077e06634831974d2710f5ea95e2c06b197281e
Jordan <jordanp@cypress.io>
2022-01-12 10:42:25 -0500

    Dx 802 create script to update readme with new image version (#578)

---
+++ browsers/CHANGELOG.md
@@ -0,0 +1,29 @@
+Name + Tag | Base image | Chrome | Firefox | Edge
+--- | --- | --- | --- | ---
+[cypress/browsers:node8.9.3-npm6.10.1-chrome75](./node8.9.3-npm6.10.1-chrome75) | `cypress/base:8.9.3-npm-6.10.1` | `75.0.3770.100` | 🚫
+[cypress/browsers:node10.16.0-chrome77-ff71](./node10.16.0-chrome77-ff71) | `cypress/browsers:node10.16.0-chrome77` | `77.0.3865.90` | `71.0`
+[cypress/browsers:node12.4.0-chrome76](./node12.4.0-chrome76) | `cypress/base:12.4.0` | `76.0.3809.87` | 🚫
+[cypress/browsers:node12.6.0-chrome75](./node12.6.0-chrome75) | `cypress/base:12.6.0` | `75.0.3770.100` | 🚫
+[cypress/browsers:node12.8.1-chrome78-ff70](./node12.8.1-chrome78-ff70) | `cypress/browsers:node12.13.0-chrome78-ff70` | `78.0.3904.97` | `70.0.1`
+[cypress/browsers:node12.13.0-chrome78-ff70](./node12.13.0-chrome78-ff70) | `cypress/base:12.13.0` | `78.0.3904.97` | `70.0.1`
+[cypress/browsers:node12.13.0-chrome80-ff74](./node12.13.0-chrome80-ff74) | `node:12.13.0-buster` | `80.0.3987.116` | `74.0`
+[cypress/browsers:node12.14.1-chrome83-ff77](./node12.14.1-chrome83-ff77) | `cypress/base:12.14.1` | `83.0.4103.61` | `77.0`
+[cypress/browsers:node12.16.1-chrome80-ff73](./node12.16.1-chrome80-ff73) | `cypress/base:12.16.1` | `80.0.3987.122` | `73.0.1`
+[cypress/browsers:node12.16.2-chrome81-ff75](./node12.16.2-chrome81-ff75) | `cypress/base:12.16.2` | `81.0.4044.113` | `75.0`
+[cypress/browsers:node12.18.0-chrome83-ff77](./node12.18.0-chrome83-ff77) | `cypress/base:12.18.0` | `83.0.4103.61` | `77.0`
+[cypress/browsers:node12.19.0-chrome86-ff82](./node12.19.0-chrome86-ff82) | `cypress/base:12.19.0` | `86.0.4240.193` | `82.0.3`
+[cypress/browsers:node12.8.1-chrome80-ff72](./node12.8.1-chrome80-ff72) | `cypress/base:node12.8.1` | `80.0.3987.87` | `72.0.2`
+[cypress/browsers:node12.18.3-chrome87-ff82](./node12.18.3-chrome87-ff82) | `cypress/base:node12.18.3` | `87.0.4280.66` | `82.0`
+[cypress/browsers:node12.18.3-chrome89-ff86](./node12.18.3-chrome89-ff86) | `cypress/base:12.18.3` | `89.0.4389.72` | `86.0.1`
+[cypress/browsers:node12.18.4-edge88](./node12.18.4-edge88) | `cypress/base:12.18.4` | 🚫 | 🚫 | `88.0.673.0 dev`
+[cypress/browsers:node13.6.0-chrome80-ff72](./node13.6.0-chrome80-ff72) | `cypress/base:13.6.0` | `80.0.3987.87` | `72.0.2`
+[cypress/browsers:node13.8.0-chrome81-ff75](./node13.8.0-chrome81-ff75) | `cypress/base:13.8.0` | `81.0.4044.113` | `75.0`
+[cypress/browsers:node14.7.0-chrome84](./node14.7.0-chrome84) | `cypress/base:14.7.0` | `84.0.4147.105` | 🚫
+[cypress/browsers:node12.14.1-chrome85-ff81](./node12.14.1-chrome85-ff81) | `cypress/base:12.14.1` | `85.0.4183.121` | `81.0`
+[cypress/browsers:node14.10.1-edge88](./node14.10.1-edge88) | `cypress/base:14.10.1` | 🚫 | 🚫 | `88.0.673.0 dev`
+[cypress/browsers:node14.15.0-chrome86-ff82](./node14.15.0-chrome86-ff82) | `cypress/base:14.15.0` | `86.0.4240.193` | `82.0.3`
+[cypress/browsers:node14.17.0-chrome88-ff89](./node14.17.0-chrome88-ff89) | `cypress/base:14.17.0` | `88.0.4324.96` | `89.0.2`
+[cypress/browsers:node14.16.0-chrome89-ff77](./node14.16.0-chrome89-ff77) | `cypress/base:14.16.0` | `89.0.4389.72` | `77.0`
+[cypress/browsers:node14.16.0-chrome89-ff86](./node14.16.0-chrome89-ff86) | `cypress/base:14.16.0` | `89.0.4389.72` | `86.0.1`
+[cypress/browsers:node14.17.0-chrome91-ff89](./node14.17.0-chrome91-ff89) | `cypress/base:14.17.0` | `91.0.4472.114` | `89.0.2`
+[cypress/browsers:node16.5.0-chrome94-ff93](./node16.5.0-chrome94-ff93) | `cypress/base:16.5.0` | `94.0.4606.71` | `93.0`
---
+++ scripts/generate-included-readme.js
@@ -0,0 +1,254 @@
+const fs = require("fs")
+const path = require("path")
+const shelljs = require("shelljs")
+const { isStrictSemver } = require("../utils")
+
+const versionTag = process.argv[2]
+const baseImageTag = process.argv[3]
+
+if (!versionTag || !isStrictSemver(versionTag)) {
+  console.error('expected Cypress version argument like "3.8.3"')
+  process.exit(1)
+}
+
+if (!baseImageTag) {
+  console.error('expected base Docker image tag like "cypress/browsers:node12.6.0-chrome77"')
+  process.exit(1)
+}
+
+if (!baseImageTag.startsWith("cypress/browsers:")) {
+  console.error('expected the base Docker image tag to be one of "cypress/browsers:*"')
+  console.error('but it was "%s"', baseImageTag)
+  process.exit(1)
+}
+
+const ReadMeDockerPulls = `
+# cypress/included
+
+[![Docker Pulls](https://img.shields.io/docker/pulls/cypress/included.svg?maxAge=604800)](https://hub.docker.com/r/cypress/included/)
+
+> Docker images with all operating system dependencies, Cypress, and some pre-installed browsers.
+`
+
+const ReadMeInstructions = `
+This image should be enough to run Cypress tests headlessly or in the interactive mode with a single Docker command like this:
+
+\`\`\`shell
+$ docker run -it -v $PWD:/e2e -w /e2e cypress/included:7.3.0
+\`\`\`
+
+## Debug
+
+If you want to see the [Cypress debug logs](https://on.cypress.io/debugging#Print-DEBUG-logs) during the run, pass environment variable \`DEBUG\`:
+
+\`\`\`shell
+$ docker run -it -v $PWD:/e2e -w /e2e -e DEBUG=cypress:* cypress/included:3.8.1
+  cypress:cli:cli cli starts with arguments ["/usr/local/bin/node","/usr/local/bin/cypress","run"] +0ms
+  cypress:cli NODE_OPTIONS is not set +0ms
+  cypress:cli:cli program parsing arguments +3ms
+  ...
+\`\`\`
+
+## Arguments
+
+These images have its entry point set to \`cypress run\` without any additional arguments. You can specify additional Cypress CLI arguments after the image name. For example to print the Help menu for the \`cypress run\` command:
+
+\`\`\`shell
+$ docker run -it --entrypoint=cypress cypress/included:7.7.0 --help
+\`\`\`
+
+To run a single spec using Chrome browser:
+
+\`\`\`shell
+$ docker run -it --entrypoint=cypress cypress/included:7.7.0 --spec cypress/integration/spec-a.js --browser chrome
+\`\`\`
+
+## Entry
+
+These images have its entry point set to \`cypress run\`. If you want to run a different command, you need to set \`--entrypoint=cypress\` and specify arguments AFTER the image name. For example, to print the Cypress information using \`cypress info\` command
+
+\`\`\`shell
+$ docker run -it --entrypoint=cypress cypress/included:4.2.0 info
+Displaying Cypress info...
+
+Detected 2 browsers installed:
+
+1. Chrome
+  - Name: chrome
+  - Channel: stable
+  - Version: 80.0.3987.116
+  - Executable: google-chrome
+
+2. Firefox
+  - Name: firefox
+  - Channel: stable
+  - Version: 74.0
+  - Executable: firefox
+
+Note: to run these browsers, pass <name>:<channel> to the '--browser' field
+
+Examples:
+- cypress run --browser firefox
+- cypress run --browser chrome
+
+Learn More: https://on.cypress.io/launching-browsers
+
+Proxy Settings: none detected
+Environment Variables:
+CYPRESS_CACHE_FOLDER: /root/.cache/Cypress
+
+Application Data: /root/.config/cypress/cy/development
+Browser Profiles: /root/.config/cypress/cy/development/browsers
+Binary Caches: /root/.cache/Cypress
+
+Cypress Version: 4.2.0
+System Platform: linux (Debian - 10.1)
+System Memory: 2.09 GB free 285 MB
+\`\`\`
+
+### Entry with arguments
+
+If you want to provide Cypress command line arguments, specify the entry point and the arguments. For example to run tests with recording and parallel mode using custom build ID "abc123" we can use:
+
+\`\`\`shell
+$ docker run -it --entrypoint=cypress cypress/included:7.5.0 \
+  run --record --parallel --ci-build-id abc123
+\`\`\`
+
+## Keep the container
+
+Every time you run \`docker run\` you spawn a new container. That container then stops after the tests finish, but there is nothing Cypress can do about it - it is the Docker command \`docker run ...\` that controls this behavior.
+
+If you are running a lot of tests again and again, you might start the container once using Bash as the entrypoint, instead of the default \`cypress\` command. Then you can execute the \`cypress run\` or any other commands, while still in the same container:
+
+\`\`\`
+$ docker run -it -v $PWD:/e2e -w /e2e \
+  --entrypoint=/bin/bash cypress/included:7.3.0
+# we are inside the container
+# let's run the tests
+root@814ed01841fe:/e2e# cypress run
+....
+# run the tests again
+root@814ed01841fe:/e2e# cypress run
+\`\`\`
+
+## Browser
+
+If you want to use a different browser (assuming it is installed in the container) use:
+
+\`\`\`shell
+$ docker run -it -v $PWD:/e2e -w /e2e --entrypoint=cypress cypress/included:3.8.1 run --browser chrome
+
+============================================================
+
+  (Run Starting)
+
+  ┌─────────────────────────────────
+  │ Cypress:    3.8.1
+  │ Browser:    Chrome 77
+  │ Specs:      1 found (spec.js)
+  └─────────────────────────────────
+
+  ...
+\`\`\`
+
+For more information, read [Run Cypress with a single Docker command](https://www.cypress.io/blog/2019/05/02/run-cypress-with-a-single-docker-command/) and [End-to-End Testing Web Apps: The Painless Way](https://mtlynch.io/painless-web-app-testing/)
+
+## Default user
+
+By default, the included images run as \`root\` user. You can switch the user to the second user in the image \`node\` or custom-mapped user, see [examples section](https://github.com/cypress-io/cypress-docker-images#examples). Starting with \`cypress/included:3.8.1\` we set permissions on the globally installed Cypress and set binary cache variable to allow other users read and execute access. Thus you will be able to run Cypress as non-root user by using \`-u node\`
+
+\`\`\`shell
+$ docker run -it -v $PWD/src:/test -w /test -u node cypress/included:3.8.1
+\`\`\`
+
+## Building and testing
+
+To build a new image use command from the root of the repo
+
+\`\`\`shell
+$ npm run add:included -- <Cypress version> <cypress/base:image>
+# example
+$ npm run add:included -- 4.0.2 cypress/browsers:node13.6.0-chrome80-ff72
+\`\`\`
+
+You should also update the \`circle.yml\` file after creating the new image with
+
+\`\`\`shell
+$ npm run build
+\`\`\`
+
+You can test the new image by creating a new project and running headless tests. For example:
+
+\`\`\`shell
+cd /tmp
+mkdir test
+cd test
+npm init --yes
+npm i -D cypress
+npx @bahmutov/cly init
+rm -rf package-lock.json package.json node_modules
+docker run -it -v $PWD:/e2e -w /e2e cypress/included:3.3.2
+\`\`\`
+
+**Tip:** the above commands are in the file [test.sh](test.sh)
+
+The tests should finish successfully using local image. Now push the image to the Docker hub
+
+\`\`\`shell
+docker push cypress/included:3.3.2
+\`\`\`
+
+## GitHub Action
+
+You can quickly run your tests in GitHub Actions using these images, see [cypress-gh-action-included](https://github.com/bahmutov/cypress-gh-action-included) repository.
+
+## GitLab CI
+
+You can use the included images to run Cypress tests on GitLab CI, see how in [cypress-example-included](https://gitlab.com/cypress-io/cypress-example-included) repository.
+
+## Wait-on
+
+If you want to run Cypress after a server has started, we suggest using [wait-on](https://github.com/jeffbski/wait-on#readme) utility. To use it from the \`cypress/included\` image, you need to disable the default entrypoint and set a new command like this:
+
+\`\`\`shell
+# execute the Cypress container once
+docker run --rm \ # remove container after finish
+  -v ./e2e:/e2e \ # map current folder to "e2e" folder
+  --workdir=/e2e \
+  --entrypoint="" \ # remove default entrypoint command
+  cypress/included:4.11.0 \
+  # wait for the local site to respond
+  # then run Cypress tests
+  /bin/bash -c 'npx wait-on http://127.0.0.1:3000 && cypress run'
+\`\`\`
+
+## Restrict CPU
+
+If you want to simulate slow container, run the Docker container with \`--cpus\` parameter, for example, let's debug the browser detection problems when the CPU is (very) slow:
+
+\`\`\`shell
+docker run -it -v $PWD:/e2e -w /e2e --cpus=0.02 \
+  -e DEBUG=cypress:launcher --entrypoint=cypress \
+  cypress/included:7.2.0 info
+\`\`\`
+`
+
+const changeLogPath = path.resolve("included", "CHANGELOG.md")
+const readmePath = path.resolve("included", "README.md")
+
+fs.readFile(changeLogPath, (err, data) => {
+  if (err) {
+    console.error(err)
+  }
+
+  const updatedChangeLog = `${data.toString()} \n[cypress/included:${versionTag}](${versionTag}) | \`${baseImageTag}\``
+
+  fs.writeFileSync(changeLogPath, updatedChangeLog.trim() + "\n", "utf8")
+  console.log("Saved CHANGELOG.md at %s", changeLogPath)
+
+  const readme = `${ReadMeDockerPulls} \n ${updatedChangeLog} \n ${ReadMeInstructions}`
+
+  fs.writeFileSync(readmePath, readme.trim() + "\n", "utf8")
+  console.log("Saved README.md at %s", readmePath)
+})

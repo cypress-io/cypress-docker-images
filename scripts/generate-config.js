2f2860b7be150112fea3e48689f6554fa120d295
Zach Bloomquist <git@chary.us>
2022-06-30 14:14:03 -0400

    push to ECR from circle

--- scripts/generate-config.js
+++ scripts/generate-config.js
@@ -16,36 +16,6 @@ if (!versionTag) {
   process.exit(1)
 }
 
-const awsCodeBuildPreamble = `version: 0.2
-env:
-    variables:
-        PUBLIC_ECR_ALIAS: "cypress-io"
-
-batch:
-    fast-fail: false
-    build-list:`
-
-const awsCodeBuildPostamble = `phases:
-    pre_build:
-        commands:
-            - aws --version
-            - echo Check if $IMAGE_TAG is in ECR...
-            - ./find-ecr-image.sh $IMAGE_REPO_NAME $IMAGE_TAG -p
-            - echo Logging in to Amazon ECR...
-            - aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-            - aws ecr-public get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin public.ecr.aws/$PUBLIC_ECR_ALIAS
-    build:
-        commands:
-            - echo Building the Docker image...
-            - cd $IMAGE_DIR/$IMAGE_TAG
-            - docker build -t $IMAGE_REPO_NAME:$IMAGE_TAG .
-            - docker tag $IMAGE_REPO_NAME:$IMAGE_TAG public.ecr.aws/$PUBLIC_ECR_ALIAS/$IMAGE_REPO_NAME:$IMAGE_TAG
-    post_build:
-        commands:
-            - echo Pushing the Docker image...
-            - docker push public.ecr.aws/$PUBLIC_ECR_ALIAS/$IMAGE_REPO_NAME:$IMAGE_TAG
-`
-
 const circleHeader = fs.readFileSync(path.join(__dirname, 'includes', 'circle-header.yml')).toString()
 
 const splitImageFolderName = (folderName) => {
@@ -122,34 +92,11 @@ const formWorkflow = (image) => {
   return yml
 }
 
-const formAwsBuildWorkflow = (image) => {
-  const identifier = camelCase(`${image.name}${image.tag}`)
-  const imageFolder = image.name === "browser" ? "browsers" : image.name
-  const job = `        - identifier: ${identifier}
-          env:
-            image: aws/codebuild/standard:5.0
-            type: LINUX_CONTAINER
-            privileged-mode: true
-            compute-type: BUILD_GENERAL1_MEDIUM
-            variables:
-                IMAGE_REPO_NAME: "cypress/${imageFolder}"
-                IMAGE_DIR: "${imageFolder}"
-                IMAGE_TAG: "${image.tag}"\n`
-  return job
-}
-
 const writeConfigFile = (image) => {
   const workflow = formWorkflow(image)
   const text = circleHeader.trim() + os.EOL + workflow
   fs.writeFileSync("circle.yml", text, "utf8")
   console.log("Generated circle.yml")
-}
-
-const writeBuildSpecConfigFile = (image) => {
-  const workflow = formAwsBuildWorkflow(image)
-  const text = awsCodeBuildPreamble.trim() + os.EOL + workflow + os.EOL + awsCodeBuildPostamble.trim()
-  fs.writeFileSync("buildspec.yml", text, "utf8")
-  console.log("Generated buildspec.yml \n")
 }
 
 const outputFolder = path.join(imageType, versionTag)
@@ -159,4 +106,3 @@ const image = splitImageFolderName(outputFolder)
 console.log("** image : %s \n", image)
 
 writeConfigFile(image)
-writeBuildSpecConfigFile(image)

cd726c05b72c7cfee04b1076ffdf214dc2ca774f
Zach Bloomquist <github@chary.us>
2022-07-15 14:33:03 -0400

    fix: updates from manual `arm64` retagging (#702)
    Co-authored-by: Adam Murray <admah@users.noreply.github.com>

--- scripts/generate-included-image.js
+++ scripts/generate-included-image.js
@@ -117,7 +117,6 @@ $ docker run -it -v $PWD:/e2e -w /e2e cypress/included:${folderName}
 # runs Cypress tests from the current folder
 \`\`\`
 
-
 **Note:** Currently, the linux/arm64 build of this image does not contain any browsers except Electron. See https://github.com/cypress-io/cypress-docker-images/issues/695 for more information.
 
 [blog post url]: https://www.cypress.io/blog/2019/05/02/run-cypress-with-a-single-docker-command/

325c389f4137c37d29e768de3a45e525db1a035b
Adam Murray <admah@users.noreply.github.com>
2022-07-25 17:27:11 -0500

    fix: update dockerName and workingDirectory values to properly reflect browsers and not browser (#713)

--- scripts/generate-config.js
+++ scripts/generate-config.js
@@ -1,30 +1,29 @@
-const path = require("path")
-const fs = require("fs")
-const { camelCase } = require("lodash")
-const os = require("os")
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
 
 const imageType = process.argv[2]
 const versionTag = process.argv[3]
 
 if (!imageType) {
-  console.error("expected an image type like included")
+  console.error('expected an image type like included')
   process.exit(1)
 }
 
 if (!versionTag) {
-  console.error("expected Cypress version argument like 3.8.3")
+  console.error('expected Cypress version argument like 3.8.3')
   process.exit(1)
 }
 
 const circleHeader = fs.readFileSync(path.join(__dirname, 'includes', 'circle-header.yml')).toString()
 
 const splitImageFolderName = (folderName) => {
-  const [name, tag] = folderName.split("/")
+  const [name, tag] = folderName.split('/')
   return { name, tag }
 }
 
 const getImageType = (image) => {
-  return image.name.includes("base") ? "base" : image.name.includes("browser") ? "browser" : "included"
+  return image.name.includes('base') ? 'base' : image.name.includes('browser') ? 'browser' : 'included'
 }
 
 const getDockerArchFromNodeArch = (nodeArch) => {
@@ -40,42 +39,48 @@ const formWorkflow = (image) => {
   const arches = ['arm64', 'x64']
 
   for (const arch of arches) {
-    yml += os.EOL + `            - build-${getImageType(image)}-image:
+    yml +=
+      os.EOL +
+      `            - build-${getImageType(image)}-image:
                 name: "build+test ${getImageType(image)} ${image.tag} ${arch}"
                 dockerTag: "${image.tag}"
                 resourceClass: ${arch === 'arm64' ? 'arm.large' : 'large'}
                 platformArg: ${getDockerArchFromNodeArch(arch)}`
 
     // add browser versions
-    if (getImageType(image) === "browser") {
-        if (image.tag.includes("-chrome")) {
+    if (getImageType(image) === 'browser') {
+      if (image.tag.includes('-chrome')) {
         yml =
-            yml +
-            `
+          yml +
+          `
                 chromeVersion: "Google Chrome ${image.tag.match(/-chrome\d*/)[0].substring(7)}"`
-        }
+      }
 
-        if (image.tag.includes("-ff")) {
+      if (image.tag.includes('-ff')) {
         yml =
-            yml +
-            `
+          yml +
+          `
                 firefoxVersion: "Mozilla Firefox ${image.tag.match(/-ff\d*/)[0].substring(3)}"`
-        }
+      }
 
-        if (image.tag.includes("-edge")) {
+      if (image.tag.includes('-edge')) {
         yml =
-            yml +
-            `
+          yml +
+          `
                 edgeVersion: "Microsoft Edge"`
-        }
+      }
     }
   }
 
-  yml += os.EOL + `            - push-images:
+  yml +=
+    os.EOL +
+    `            - push-images:
                 name: "push ${getImageType(image)} ${image.tag} images"
-                dockerName: 'cypress/${getImageType(image)}'
+                dockerName: 'cypress/${getImageType(image) === 'browser' ? 'browsers' : getImageType(image)}'
                 dockerTag: '${image.tag}'
-                workingDirectory: '~/project/${getImageType(image)}/${image.tag}'
+                workingDirectory: '~/project/${getImageType(image) === 'browser' ? 'browsers' : getImageType(image)}/${
+      image.tag
+    }'
                 context: test-runner:docker-push
                 requires:`
 
@@ -89,14 +94,14 @@ const formWorkflow = (image) => {
 const writeConfigFile = (image) => {
   const workflow = formWorkflow(image)
   const text = circleHeader.trim() + os.EOL + workflow
-  fs.writeFileSync("circle.yml", text, "utf8")
-  console.log("Generated circle.yml")
+  fs.writeFileSync('circle.yml', text, 'utf8')
+  console.log('Generated circle.yml')
 }
 
 const outputFolder = path.join(imageType, versionTag)
-console.log("** outputFolder : %s", outputFolder)
+console.log('** outputFolder : %s', outputFolder)
 
 const image = splitImageFolderName(outputFolder)
-console.log("** image : %s \n", image)
+console.log('** image : %s \n', image)
 
 writeConfigFile(image)

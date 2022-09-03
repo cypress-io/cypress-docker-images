ddabbcd6690275d8dd55e23873c053a53af13255
sch0rsch <schorsch@wg-e26.de>
2022-09-03 11:38:01 +0200

    fix edge installation in generated browser Dockerfile

--- scripts/generate-browser-image.js
+++ scripts/generate-browser-image.js
@@ -71,14 +71,14 @@ RUN node -p "process.arch === 'arm64' ? 'Not downloading Firefox since we are on
 
 const edgeDownload = `
 RUN node -p "process.arch === 'arm64' ? 'Not downloading Edge since we are on arm64: https://techcommunity.microsoft.com/t5/discussions/edge-for-linux-arm64/m-p/1532272' : process.exit(1)" || \\
-  (curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg \\
+  (curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && \\
   install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/ && \\
   sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list' && \\
   rm microsoft.gpg && \\
   ## Install Edge
   apt-get update && \\
   apt-get install -y microsoft-edge-dev && \\
-  ## Add a link to the browser that allows Cypress to find it && \\
+  ## Add a link to the browser that allows Cypress to find it
   ln -s /usr/bin/microsoft-edge /usr/bin/edge)`
 
 const Dockerfile = `

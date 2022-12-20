399b5cf1127c5ba40fcef3ca8b302722be4baf5c
mjhenkes <mjhenkes@gmail.com>
2022-12-20 10:16:42 -0600

    Fixes for installing chrome and cypress standalone

--- factory/installScripts/chrome/default.sh
+++ factory/installScripts/chrome/default.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-# Chrome offers a debian package, I wasn't able to add the package list due to signing issues so isntead we downlaod the specific deb for the requested version, then 'fix' the package by installing missing dependencies.
-wget --no-verbose -O /usr/src/google-chrome-stable_current_amd64.deb http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${1}_amd64.deb \
-  && dpkg -i /usr/src/google-chrome-stable_current_amd64.deb ;
+# Chrome offers a debian package, download the specific debian package and install it with apt-get to also install dependencies.
+wget --no-verbose -O /usr/src/google-chrome-stable_current_amd64.deb http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${1}_amd64.deb
 
-# install any missing dependancies
-apt-get install -f -y
+apt-get update
+apt-get install -f -y /usr/src/google-chrome-stable_current_amd64.deb
+
 # remove temp download
-rm -f /usr/src/google-chrome-stable_current_amd64.deb
+rm -f /usr/src/google-chrome-stable_current_amd64.debd

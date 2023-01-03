22e1af553d1bd4a0633513345234cccd30da31e5
mjhenkes <mjhenkes@gmail.com>
2023-01-03 09:15:53 -0600

    use edge stable

--- factory/installScripts/edge/default.sh
+++ factory/installScripts/edge/default.sh
@@ -6,8 +6,8 @@ apt-get update \
     ca-certificates \
   && curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg \
   && install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d \
-  && sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list' \
+  && sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-stable.list' \
   && rm microsoft.gpg \
   && apt-get update \
-  && apt-get install -y microsoft-edge-dev=${1} \
+  && apt-get install -y microsoft-edge-stable=${1} \
   && ln -s /usr/bin/microsoft-edge /usr/bin/edge

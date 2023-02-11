a74fc87422acb74ecaa5925e0985917753c9b271
Jonathan Stacks <jonstacks13@gmail.com>
2023-02-11 02:56:55 -0600

    Fix binary_state.json file note existing when running as node user

--- factory/installScripts/cypress/default.sh
+++ factory/installScripts/cypress/default.sh
@@ -3,3 +3,8 @@
 # TODO: should typescript be versioned? Should it have it's own ARG for the factory?
 # Typescript is installed to allow testing of .ts spec files.
 npm install -g "cypress@${1}" typescript
+
+# Run cypress verify to create the binary_state.json file. The node user
+# has access to read /root/.cache/Cypress/<version>/binary_state.json but
+# not to write it
+cypress verify

7c971ac8e087dd69e2724c7ba03b34346f60f6ef
mjhenkes <mjhenkes@gmail.com>
2023-02-13 16:05:28 -0600

    add a wordy comment

--- factory/installScripts/cypress/default.sh
+++ factory/installScripts/cypress/default.sh
@@ -4,4 +4,10 @@
 # Typescript is installed to allow testing of .ts spec files.
 npm install -g "cypress@${1}" typescript
 
+# Loosen file priveleges for the cypress cache. The first time that cypress runs it will create a
+# binary_state.json file if it hasn't already been created. This was causing issues with non-root
+# users, they do not have access to write to this directory. Since this is a develompent docker container
+# and to lower barriors as much as possible, we are loosening privs to allow the binary_state.json file
+# to be created. Previously this file was created by root when cypress verify was called, but this would
+# apply to amd processors since cypress verify was not called on arm processors.
 chmod -R 777 /root/.cache

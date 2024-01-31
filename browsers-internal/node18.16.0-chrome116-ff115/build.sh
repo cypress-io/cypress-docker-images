b3f30647bb2e67f29ba295440eab967eb264d3d4
Cacie Prins <cacieprins@users.noreply.github.com>
2024-01-31 15:38:21 -0500

    Chore: Node 18.17.1, Chrome 118 internal images (#1009)
    * chore: internal docker images for node 18.16 and chrome 116
    
    * update build comments, consolidate edge image
    
    * add node 18.17 base-internal
    
    * various browsers-internal images
    
    * 18.17.1 base internal docker cfg
    
    * Update base-internal/18.17.0/README.md
    
    Co-authored-by: Bill Glesias <bglesias@gmail.com>
    
    * docs, scripts fixes
    
    * Update browsers-internal/node18.16.0-chrome116-ff115-edge/README.md
    
    Co-authored-by: Bill Glesias <bglesias@gmail.com>
    
    * Update browsers-internal/node18.17.1-chrome118-ff115/README.md
    
    Co-authored-by: Bill Glesias <bglesias@gmail.com>
    
    * Update browsers-internal/node18.17.0-chrome118-ff115/README.md
    
    Co-authored-by: Bill Glesias <bglesias@gmail.com>
    
    ---------
    
    Co-authored-by: Bill Glesias <bglesias@gmail.com>

---
+++ browsers-internal/node18.16.0-chrome116-ff115/build.sh
@@ -0,0 +1,5 @@
+set e+x
+
+LOCAL_NAME=cypress/browsers-internal:node18.16.0-chrome116-ff115
+echo "Building $LOCAL_NAME"
+docker build -t $LOCAL_NAME .
---
+++ browsers-internal/node18.17.1-chrome118-ff115-edge/build.sh
@@ -0,0 +1,5 @@
+set e+x
+
+LOCAL_NAME=cypress/browsers-internal:node18.17.1-chrome118-ff115-edge
+echo "Building $LOCAL_NAME"
+docker build -t $LOCAL_NAME .

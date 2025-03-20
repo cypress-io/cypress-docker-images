384f1f3601272003b9da3c76bb0efcfbec4cae4e
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2025-03-20 17:03:27 +0100

    chore: add eslint linting for JavaScript

--- factory/installScripts/cypress/install-cypress-version.js
+++ factory/installScripts/cypress/install-cypress-version.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
-const { spawn } = require('child_process');
+const { spawn } = require('child_process')
 
 const cypressVersion = process.argv.slice(2)[0]
 
 if (!cypressVersion) {
   console.log('No Cypress version provided, skipping Cypress install')
-  return
+  process.exit(0)
 }
 
 console.log('Installing Cypress version ', cypressVersion)
 
 // Insert logic here if needed to run a different install script based on cypress version.
-const install = spawn(`${__dirname}/default.sh`, [cypressVersion], {stdio: 'inherit'})
+const install = spawn(`${__dirname}/default.sh`, [cypressVersion], { stdio: 'inherit' })
 
 install.on('error', function (error) {
   console.log('child process errored with ' + error.toString())
   process.exit(1)
-});
+})
 
 install.on('exit', function (code) {
   console.log('child process exited with code ' + code.toString())
   process.exit(code)
-});
+})
--- factory/installScripts/yarn/install-yarn-version.js
+++ factory/installScripts/yarn/install-yarn-version.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const { spawn } = require('child_process');
+const { spawn } = require('child_process')
 
 const yarnVersion = process.argv.slice(2)[0]
 
 if (!yarnVersion) {
   console.log('No Yarn version provided, skipping Yarn install')
-  return
+  process.exit(0)
 }
 
 if (yarnVersion >= '2') {
@@ -16,14 +16,14 @@ if (yarnVersion >= '2') {
 console.log('Installing Yarn version: ', yarnVersion)
 
 // Insert logic here if needed to run a different install script based on version.
-const install = spawn(`${__dirname}/default.sh`, [yarnVersion], {stdio: 'inherit'})
+const install = spawn(`${__dirname}/default.sh`, [yarnVersion], { stdio: 'inherit' })
 
 install.on('error', function (error) {
   console.log('child process errored with ' + error.toString())
   process.exit(1)
-});
+})
 
 install.on('exit', function (code) {
   console.log('child process exited with code ' + code.toString())
   process.exit(code)
-});
+})

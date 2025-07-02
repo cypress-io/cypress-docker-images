2ae99bc6a8cec5eb05cdb0a5ce48cdb28ae0c118
Cacie Prins <cacie@cypress.io>
2025-07-02 13:25:15 -0400

    rm electron-35 related images

--- browsers-internal/releases/node-20/node20.19.1-bullseye-chrome132-ff138-edge/global-profile.sh
+++
@@ -1,4 +0,0 @@
-# set environment variables for the image
-export CHROME_BIN=/usr/bin/google-chrome
-export FIREFOX_BIN=/usr/bin/firefox
-export EDGE_BIN=/usr/bin/microsoft-edge 
--- browsers-internal/releases/node-20/node20.19.1-chrome132-ff138/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-LOCAL_NAME=cypress/browsers-internal:node20.19.1-chrome132-ff138
-echo "Building $LOCAL_NAME"
-
-docker build -t $LOCAL_NAME --platform linux/amd64 . 

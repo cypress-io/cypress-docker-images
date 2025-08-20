9187a6fb2f19eec9f2d24548bcbbcbd22ec2c48c
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2025-08-20 08:58:52 +0200

    update to PR 32288

--- factory/installScripts/cypress/install.sh
+++ factory/installScripts/cypress/install.sh
@@ -3,7 +3,7 @@ set -e
 # TODO: should typescript be versioned? Should it have it's own ARG for the factory?
 # Typescript is installed to allow testing of .ts spec files.
 if [[ -n "$1" ]]; then
-  npm install -g https://cdn.cypress.io/beta/npm/15.0.0/linux-x64/develop-f6e0744e38f6b86af49ea3b036bedbe45a672008/cypress.tgz typescript
+  npm install -g https://cdn.cypress.io/beta/npm/15.0.0/linux-x64/develop-6db5cb19350db11fe615dfc17a07d09b46a4d8d1/cypress.tgz typescript
 
   # Loosen file priveleges for the cypress cache. The first time that cypress runs it will create a
   # binary_state.json file if it hasn't already been created. This was causing issues with non-root

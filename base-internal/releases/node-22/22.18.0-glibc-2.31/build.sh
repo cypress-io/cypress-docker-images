1202dac6e5f62168a7c050947f1b8c30d12fce9d
Jennifer Shehane <shehane.jennifer@gmail.com>
2025-08-27 14:49:57 -0400

    chore: publish base-internal with node 22.18.0

---
+++ base-internal/releases/node-22/22.18.0-glibc-2.31/build.sh
@@ -0,0 +1,7 @@
+set e+x
+
+# build image with Cypress dependencies
+LOCAL_NAME=cypress/base-internal:22.18.0-bullseye-glibc-2.31
+
+echo "Building $LOCAL_NAME"
+docker build -t $LOCAL_NAME . --platform linux/amd64

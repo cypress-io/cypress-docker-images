9a5584e59917c2d3ae4fcf5aa1ae2dcfa6216be2
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2025-09-09 23:22:37 +0200

    fix: use microsoft-edge.list for Edge installation (#1406)
    * fix(microsoft-edge): use same apt source filename as Microsoft post-install script
    
    * fix: use microsoft-edge.list for Edge installation
    
    * Update Chrome browsers
    
    CHROME_VERSION to 140.0.7339.80-1
    CHROME_FOR_TESTING_VERSION to 140.0.7339.80
    
    * update Edge to 140.0.3485.54-1
    
    ---------
    
    Co-authored-by: Marco Reyes R <marcoreyes@falabella.cl>

--- factory/installScripts/edge/default.sh
+++ factory/installScripts/edge/default.sh
@@ -3,7 +3,7 @@
 # Microsoft offers a debian package, here we're adding the package list and then installing the specific version we want to install.
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg \
   && install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d \
-  && sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-stable.list' \
+  && sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge.list' \
   && rm microsoft.gpg \
   && apt-get update \
   && apt-get install -y microsoft-edge-stable=${1} \

f57efd85bf82f74898e91d6b49f4ce581045bbf6
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2025-09-25 15:51:02 +0200

    chore(deps): migrate to ESLint extends config structure (#1426)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,26 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
-import pluginJs from '@eslint/js'
+import js from '@eslint/js'
 import pluginCypress from 'eslint-plugin-cypress'
 import stylistic from '@stylistic/eslint-plugin'
 
-export default [
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  pluginCypress.configs.recommended,
-  stylistic.configs.recommended,
-  { ignores: ['factory/test-project/**/*.js'] },
+export default defineConfig([
+  globalIgnores(['factory/test-project/**/*.js']),
   {
+    files: ['**/*.{,m}js'],
+    extends: [
+      js.configs.recommended,
+      pluginCypress.configs.recommended,
+      stylistic.configs.recommended,
+    ],
     rules: {
       '@stylistic/indent': ['error', 2],
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
       '@stylistic/quotes': ['error', 'single'],
       '@stylistic/semi': ['error', 'never'],
     },
+    languageOptions: {
+      globals: globals.node,
+    },
   },
-]
+])

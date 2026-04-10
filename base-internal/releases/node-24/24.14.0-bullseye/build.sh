5be5598e2de4e9ff9ca757047fbd4d26816eb0e8
Matthew Schile <mschile@cypress.io>
2026-04-10 13:20:41 -0600

    remove bullseye

--- base-internal/releases/node-24/24.14.0-bullseye/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e+x
-
-# build image with Cypress dependencies
-LOCAL_NAME=cypress/base-internal:24.14.0-bullseye
-
-echo "Building $LOCAL_NAME"
-docker build -t $LOCAL_NAME . --platform linux/amd64

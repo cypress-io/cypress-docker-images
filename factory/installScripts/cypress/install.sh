b744e11ed861addb437ba58aafc4a00132418330
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2026-04-17 10:57:31 +0200

    fix(deps): pin typescript to v6 for cypress install
    In Cypress install script:
    - correct typos
    - update message text to align with other installation scripts
    - add log for version of TypeScript installed
    - add TypeScript installation info to factory README
    
    Update
    FACTORY_VERSION to 8.0.2

--- factory/installScripts/cypress/install.sh
+++ factory/installScripts/cypress/install.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 set -e
-# TODO: should typescript be versioned? Should it have it's own ARG for the factory?
+# TODO: Should typescript have its own ARG for the factory?
 # Typescript is installed to allow testing of .ts spec files.
 if [[ -n "$1" ]]; then
-  npm install -g "cypress@$1" typescript
+  npm install -g "cypress@$1" typescript@6
+  echo "Installed TypeScript $(tsc --version)"
 
-  # Loosen file priveleges for the cypress cache. The first time that cypress runs it will create a
-  # binary_state.json file if it hasn't already been created. This was causing issues with non-root
-  # users, they do not have access to write to this directory. Since this is a develompent docker container
-  # and to lower barriers as much as possible, we are loosening privs to allow the binary_state.json file
+  # Loosen file privileges for the cypress cache. The first time that cypress runs, it will create a
+  # binary_state.json file, if it hasn't already been created. This was causing issues with non-root
+  # users who do not have access to write to this directory. Since this is a development docker container
+  # and to lower barriers as much as possible, privileges are loosened to allow the binary_state.json file
   # to be created. Previously this file was created by root when cypress verify was called, but this would
   # apply to amd processors since cypress verify was not called on arm processors.
   chmod -R 777 /root/.cache
 else
-  echo 'No Cypress version provided; skipping install.' 
+  echo 'No Cypress version provided, skipping Cypress install'
 fi

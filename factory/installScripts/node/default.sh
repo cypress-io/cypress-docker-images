f58c722e5968aa33cd990d8358267e9716c3e56d
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2026-07-12 17:24:10 +0200

    chore(factory): remove node install script unsupported architectures
    Remove
    ppc64el / ppc64le
    s390x
    armhf / armv7l (32 bit)
    i386 / x86` (32 bit)

--- factory/installScripts/node/default.sh
+++ factory/installScripts/node/default.sh
@@ -7,11 +7,7 @@ groupadd --gid 1000 node \
 ARCH= && dpkgArch="$(dpkg --print-architecture)" \
     && case "${dpkgArch##*-}" in \
       amd64) ARCH='x64';; \
-      ppc64el) ARCH='ppc64le';; \
-      s390x) ARCH='s390x';; \
       arm64) ARCH='arm64';; \
-      armhf) ARCH='armv7l';; \
-      i386) ARCH='x86';; \
       *) echo "unsupported architecture"; exit 1 ;; \
     esac \
     && set -ex \

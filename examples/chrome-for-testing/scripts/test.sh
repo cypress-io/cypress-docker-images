e4e6a84caa137babc90137d1d22a9448fcf41b13
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2026-09-09 09:31:12 +0200

    Use Build-kit TARGETARCH

--- examples/chrome-for-testing/scripts/test.sh
+++ examples/chrome-for-testing/scripts/test.sh
@@ -6,24 +6,6 @@ set -e # fail on error
 #
 # Run ./scripts/test.sh in directory examples/chrome-for-testing
 #
-ARCHITECTURE=$(uname -m)
-echo Running on "$ARCHITECTURE"
-
-case $ARCHITECTURE in
-  x86_64)
-    echo Testing Chrome for Testing in amd64
-    platformFilename=chrome-linux64
-    ;;
-  aarch64)
-    echo Testing Chrome for Testing in arm64
-    platformFilename=chrome-linux-arm64
-    ;;
-  *)
-    echo Unsupported architecture
-    exit 1
-    ;;
-esac
-
 npm ci # Install dependencies
 # uncomment lines below to test different versions of Chrome for Testing
 # See https://googlechromelabs.github.io/chrome-for-testing/ for current versions
@@ -39,7 +21,7 @@ chromeVersion=(
 for i in "${!chromeVersion[@]}"; do
 echo
 echo CHROME_VERSION "${chromeVersion[$i]}"
-docker build --build-arg CHROME_VERSION="${chromeVersion[$i]}" --build-arg PLATFORM_FILENAME=${platformFilename} -t test-chrome-for-testing .
+docker build --build-arg CHROME_VERSION="${chromeVersion[$i]}" -t test-chrome-for-testing .
 docker run --rm --entrypoint bash test-chrome-for-testing -c "npx cypress info"
 docker run --rm --entrypoint bash test-chrome-for-testing -c "npx cypress run --browser chrome-for-testing"
 done
